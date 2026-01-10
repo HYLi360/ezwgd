fd41671bfbf7b7953797374e285c9249e425452b
hyli360 <lihuanyu2003@gmail.com>
2026-01-10 17:11:14 +0800

    nightly build

--- src/ezwgd/frontend/frontend.py
+++ src/ezwgd/frontend/frontend.py
@@ -7,11 +7,6 @@ import subprocess
 import os
 
 
-from typing import Literal
-
-
-
-
 def gffread_cds_pep_extract(
         gff3_file_path: str,
         fna_file_path: str,
@@ -41,37 +36,6 @@ def gffread_cds_pep_extract(
     subprocess.run([abs_gffread_path, gff3_file_path, '-g', fna_file_path, '-y', pep_out],
                    cwd=workdir)
     os.remove(f'{workdir}/{fna_file_path}.fai')
-
-
-class Diamond:
-    def __init__(
-            self,
-            fasta_query_path: str,
-            fasta_target_path: str,
-            diamond_path: str = 'diamond',
-            verbose: bool = False,
-            threads: int = 6,
-            min_evalue: float = 1e-3,
-            max_target_seqs: int = 25,
-            sensitivity: Literal[
-                'faster', 'fast', 'mid-sensitive', 'sensitive',
-                'more-sensitive', 'very-sensitive', 'ultra-sensitive'] = 'ultra-sensitive',
-            outfmt: int = 6,
-            no_self_hits: bool = True,
-    ) -> None:
-        self.tmpdir = ''
-        self.makedb_command = [
-            diamond_path,
-            'makedb',
-            f'--threads {threads}',
-            f'--db {self.tmpdir}/diamond.db',
-            f'--verbose {verbose}',
-        ]
-        self.diamond_command = [
-            diamond_path,
-            'blastp',
-            f'--threads {threads}',
-        ]
 
 
 def muscle5(
